92008856aae856db3d4e326b81132ab0bd812ebb
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-29 15:11:07 +0100

    Display and style data

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,16 +1,40 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchMissions } from '../redux/mission/missionsSlice';
 
 const Mission = () => {
   const dispatch = useDispatch();
+  const missions = useSelector((store) => store.missions.missions);
+
   useEffect(() => {
     dispatch(fetchMissions());
   });
+
+  const mission = missions.map((item) => (
+    <tr key={item.id}>
+      <td className="fw-bold">{ item.name }</td>
+      <td>{ item.desc }</td>
+      <td className="align-middle text-center"><span>NOT A MEMBER</span></td>
+      <td className="align-middle text-center"><button type="button"> Join Mission </button></td>
+    </tr>
+  ));
+
   return (
-    <>
-      <div>Mission</div>
-    </>
+    <section className="m-3 mt-0 p-3 pt-0 table-responsive">
+      <table className="table table-striped table-bordered">
+        <thead>
+          <tr>
+            <th width="150">Mission</th>
+            <th>Description</th>
+            <th width="150">Status</th>
+            <th width="150"> </th>
+          </tr>
+        </thead>
+        <tbody>
+          {mission}
+        </tbody>
+      </table>
+    </section>
 
   );
 };
