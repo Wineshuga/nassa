744273cfeb4252fdf524c05b4527d587213c8990
Fatima Najafi <najafifatima762@gmail.com>
2023-08-29 15:18:09 +0430

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import rocketsReducer from './rocketsSlice';
 import missionsReducer from './mission/missionsSlice';
 
-
 const store = configureStore({
   reducer: {
     rockets: rocketsReducer,
