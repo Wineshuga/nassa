15dabc4fc86ac238b53ce0093744e558cd8a5473
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-29 11:33:52 +0100

    Fetch missions data

--- src/redux/mission/missionsSlice.jsx
+++ src/redux/mission/missionsSlice.jsx
@@ -1,4 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const url = 'https://api.spacexdata.com/v3/missions';
+
+export const fetchMissions = createAsyncThunk('type/fetchMissions', async (_, thunkAPI) => {
+  try {
+    const response = await axios.get(url);
+    return response.data;
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err);
+  }
+});
 
 const initialState = [];
 
