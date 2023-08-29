f05f59c814e1ebac645a141cfb187bf14aab8aea
Fatima Najafi <najafifatima762@gmail.com>
2023-08-29 19:00:59 +0430

    Update

--- src/components/Rockets.jsx
+++ src/components/Rockets.jsx
@@ -2,63 +2,92 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import '../styles/rockets.css';
-import { getAPI } from '../redux/rocketsSlice';
+import { getAPI, reservation } from '../redux/rocketsSlice';
 
 const RocketList = () => {
   const dispatch = useDispatch();
   const { RocketList, isLoading, error } = useSelector(
     (store) => store.rockets,
   );
-
   useEffect(() => {
     dispatch(getAPI());
   }, [dispatch]);
-
   if (isLoading === true) {
     return <div>Loading</div>;
   }
-
   if (isLoading === false) {
     return (
       <div data-testid="RocketList">
         {RocketList.map((item) => (
           <Rocket
             key={item.id}
-            id={item.id}
             name={item.name}
-            description={item.description}
-            images={item.flickr_images}
+            disc={
+              item.reserved ? (
+                <div>
+                  <span className="reserved">Reserved </span>
+                  {item.disc}
+                </div>
+              ) : (
+                item.disc
+              )
+            }
+            image={item.images}
+            Reservation={item.id}
+            reservationState={
+              item.reserved ? (
+                <button type="button" className="Cancel-Reservation">
+                  Cancel Reservation
+                </button>
+              ) : (
+                <button type="button" className="Reservation">
+                  Reserve rocket
+                </button>
+              )
+            }
           />
         ))}
       </div>
     );
   }
-
   return <div>{error}</div>;
 };
 
 const Rocket = (props) => {
+  const dispatch = useDispatch();
+
   const
     {
-      id, name, description, images,
+      id, name, disc, image, Reservation, reservationState,
     } = props;
-
+  /* eslint-disable */
   return (
     <div className="rocketC" key={id}>
-      <img className="rocketImg" src={images[1]} alt={name} />
+      <img className="rocketImg" src={image} alt={name} />
       <div className="contentC">
-        <h4 className="rocketName">{name}</h4>
-        <div className="rocketDesc">{description}</div>
+        <h4 className="rocektName">{name}</h4>
+        <div className="rocketDesc">{disc}</div>
+        <div
+          type="button"
+          onClick={() => {
+            dispatch(reservation(Reservation));
+          }}
+        >
+          {reservationState}
+        </div>
       </div>
     </div>
+    /* eslint-enable */
   );
 };
 
 Rocket.propTypes = {
   id: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
-  images: PropTypes.arrayOf(PropTypes.string).isRequired,
+  disc: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  Reservation: PropTypes.number.isRequired,
+  reservationState: PropTypes.number.isRequired,
 };
 
 export default RocketList;
--- src/redux/rocketsSlice.jsx
+++ src/redux/rocketsSlice.jsx
@@ -4,12 +4,12 @@ import axios from 'axios';
 
 const url = 'https://api.spacexdata.com/v3/rockets';
 
-export const getAPI = createAsyncThunk('rockets/fetchRocket', async () => {
+export const getAPI = createAsyncThunk('Rockets/fetchRocket', async () => {
   try {
     const response = await axios.get(url);
     return response.data;
   } catch (err) {
-    throw new Error(err.message);
+    return err.message;
   }
 });
 
@@ -22,26 +22,52 @@ const initialState = {
 const rocketsSlice = createSlice({
   name: 'rockets',
   initialState,
-  reducers: {},
+  reducers: {
+    reservation: (state, action) => {
+      const id = action.payload;
+      state.RocketList = state.RocketList.map((rocket) => {
+        if (rocket.id !== id) {
+          return rocket;
+        }
+        if (rocket.id === id) {
+          if (rocket.reserved === true) {
+            return { ...rocket, reserved: false };
+          }
+        }
+        return { ...rocket, reserved: true };
+      });
+    },
+  },
   extraReducers(builder) {
     builder
       .addCase(getAPI.pending, (state) => {
         state.isLoading = true;
       })
       .addCase(getAPI.fulfilled, (state, action) => {
-        state.RocketList = action.payload.map((item) => ({
-          id: item.id,
-          name: item.rocket_name,
-          description: item.description,
-          flickr_images: item.flickr_images,
-        }));
+        const rockets = [];
+        action.payload.map((item) => {
+          let newRocket = {};
+          newRocket = {
+            id: item.id,
+            name: item.rocket_name,
+            disc: item.description,
+            images: item.flickr_images[1],
+            reserved: false,
+          };
+          return rockets.push(newRocket);
+        });
+        state.RocketList = rockets;
         state.isLoading = false;
       })
       .addCase(getAPI.rejected, (state, action) => {
         state.isLoading = false;
+        state.RocketList = [];
+
         state.error = action.error.message;
       });
   },
 });
+
+export const { reservation } = rocketsSlice.actions;
 
 export default rocketsSlice.reducer;
