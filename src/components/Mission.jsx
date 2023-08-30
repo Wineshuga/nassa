5a064cdd1b086b8cf33cf428acba53065132c2c2
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-30 11:35:29 +0100

    Dispatch joinMission reducer function onClick

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchMissions } from '../redux/mission/missionsSlice';
+import { fetchMissions, joinMission } from '../redux/mission/missionsSlice';
 
 const Mission = () => {
   const dispatch = useDispatch();
@@ -8,14 +8,24 @@ const Mission = () => {
 
   useEffect(() => {
     dispatch(fetchMissions());
-  });
+  }, [dispatch]);
 
   const mission = missions.map((item) => (
     <tr key={item.id}>
       <td className="fw-bold">{ item.name }</td>
       <td>{ item.desc }</td>
       <td className="align-middle text-center"><span>NOT A MEMBER</span></td>
-      <td className="align-middle text-center"><button type="button"> Join Mission </button></td>
+      <td className="align-middle text-center">
+        <button
+          type="button"
+          onClick={() => dispatch(joinMission({ id: item.id }))}
+        >
+          {' '}
+          Join Mission
+          {' '}
+
+        </button>
+      </td>
     </tr>
   ));
 
