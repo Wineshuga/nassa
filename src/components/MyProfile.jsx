8c361fa150abdbb84738652797548f66577360f4
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-30 16:52:31 +0100

    Add structure to profile

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useSelector } from 'react-redux';
 
-function MyProfile() {
-  return <div>MyProfile</div>;
-}
+const MyProfile = () => {
+  const { missions } = useSelector((store) => store.missions);
+  return (
+    <div className="container d-flex">
+      <section className="">
+        <h3>My Missions</h3>
+        <ul className="list-group">
+          {missions.filter((item) => item.reserved)
+            .map((item) => (
+              <li
+                key={item.id}
+                className="list-group-item p-4"
+              >
+                {item.name}
+              </li>
+            ))}
+        </ul>
+      </section>
+    </div>
+  );
+};
 
 export default MyProfile;
