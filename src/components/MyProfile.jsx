d74dc8f96223acc044b7fda3c9ab609619dfb921
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-30 17:50:08 +0100

    Add reserved missions to profile

--- src/components/MyProfile.jsx
+++ src/components/MyProfile.jsx
@@ -1,35 +1,68 @@
-// import React from 'react';
 import { useSelector } from 'react-redux';
-import { Container, ListGroup } from 'react-bootstrap';
+import {
+  Container, ListGroup, Row, Col,
+} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/rockets.css';
 
 const Rocket = () => {
   const rockets = useSelector((state) => state.rockets);
+
   const myRockets = rockets.RocketList.filter((rocket) => rocket.reserved === true);
   if (myRockets.length === 0) {
     return (
-      <Container>
+      <Col>
         <h3>My Rockets</h3>
         You have not reserved any rockets.
-      </Container>
+      </Col>
     );
   }
   return (
-    <Container>
+    <Col>
       <h3>My Rockets</h3>
-      <ListGroup as="ol" numbered>
+      <ListGroup as="ol">
         {myRockets.map((rocket) => (
           <ListGroup.Item as="li" key={rocket.id}>{rocket.name}</ListGroup.Item>
         ))}
       </ListGroup>
-    </Container>
+    </Col>
+  );
+};
+
+const Mission = () => {
+  const { missions } = useSelector((store) => store.missions);
+
+  const myMissions = missions.filter((item) => item.reserved);
+  if (myMissions.length === 0) {
+    return (
+      <Col>
+        <h3>My Missions</h3>
+        <p>You have not reserved any missions.</p>
+      </Col>
+    );
+  } return (
+    <Col>
+      <h3>My Missions</h3>
+      <ListGroup as="ol">
+        {myMissions.map((item) => (
+          <ListGroup.Item
+            as="li"
+            key={item.id}
+          >
+            {item.name}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
+    </Col>
   );
 };
 
 const MyProfile = () => (
-  <Container className="d-flex">
-    <Rocket />
+  <Container>
+    <Row>
+      <Mission />
+      <Rocket />
+    </Row>
   </Container>
 );
 
