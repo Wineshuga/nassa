5c24c68ab74f2e7b3993bedc9d26fb360d321730
Fatima Najafi <najafifatima762@gmail.com>
2023-08-31 16:30:02 +0430

    Update

--- src/redux/rocketsSlice.jsx
+++ src/redux/rocketsSlice.jsx
@@ -1,12 +1,91 @@
+// import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+
+// const url = 'https://api.spacexdata.com/v3/rockets';
+
+// export const getAPI = createAsyncThunk('Rockets/fetchRocket', async () => {
+//   try {
+//     const response = await axios.get(url);
+//     return response.data;
+//   } catch (err) {
+//     return err.message;
+//   }
+// });
+
+// const initialState = {
+//   RocketList: [],
+//   isLoading: true,
+//   error: undefined,
+// };
+
+// const rocketsSlice = createSlice({
+//   name: 'rockets',
+//   initialState,
+//   reducers: {
+//     reservation: (state, action) => {
+//       const { payload: id } = action;
+//       const updatedRocketList = state.RocketList.map((rocket) => {
+//         if (rocket.id !== id) {
+//           return rocket;
+//         }
+//         return { ...rocket, reserved: !rocket.reserved };
+//       });
+
+//       return {
+//         ...state,
+//         RocketList: updatedRocketList,
+//       };
+//     },
+//   },
+//   extraReducers(builder) {
+//     builder
+//       .addCase(getAPI.pending, (state) => ({
+//         ...state,
+//         isLoading: true,
+//       }))
+//       .addCase(getAPI.fulfilled, (state, action) => {
+//         const rockets = [];
+//         action.payload.map((item) => {
+//           let newRocket = {};
+//           newRocket = {
+//             id: item.id,
+//             name: item.rocket_name,
+//             disc: item.description,
+//             images: item.flickr_images[1],
+//             reserved: false,
+//           };
+//           return rockets.push(newRocket);
+//         });
+//         return {
+//           ...state,
+//           RocketList: rockets,
+//           isLoading: false,
+//         };
+//       })
+//       .addCase(getAPI.rejected, (state, action) => ({
+//         ...state,
+//         isLoading: false,
+//         RocketList: [],
+//         error: action.error.message,
+//       }));
+//   },
+// });
+
+// export const { reservation } = rocketsSlice.actions;
+
+// export default rocketsSlice.reducer;
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 
 const url = 'https://api.spacexdata.com/v3/rockets';
 
 export const getAPI = createAsyncThunk('Rockets/fetchRocket', async () => {
   try {
-    const response = await axios.get(url);
-    return response.data;
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      return data;
+    }
+    throw new Error('Failed to fetch rockets');
   } catch (err) {
     return err.message;
   }
@@ -44,18 +123,14 @@ const rocketsSlice = createSlice({
         isLoading: true,
       }))
       .addCase(getAPI.fulfilled, (state, action) => {
-        const rockets = [];
-        action.payload.map((item) => {
-          let newRocket = {};
-          newRocket = {
-            id: item.id,
-            name: item.rocket_name,
-            disc: item.description,
-            images: item.flickr_images[1],
-            reserved: false,
-          };
-          return rockets.push(newRocket);
-        });
+        const rockets = action.payload.map((item) => ({
+          id: item.id,
+          name: item.rocket_name,
+          disc: item.description,
+          images: item.flickr_images[1],
+          reserved: false,
+        }));
+
         return {
           ...state,
           RocketList: rockets,
