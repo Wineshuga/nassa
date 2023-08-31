d85f10bfad8340e724fe1917f6be83778e26e99d
Wineshuga <uzochukwuwinnie@gmail.com>
2023-08-31 14:17:38 +0100

    Add line after header

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -8,30 +8,33 @@ const links = [
   { path: '/MyProfile', text: 'My Profile' },
 ];
 const Nav = () => (
-  <Container className="d-flex flex-wrap align-items-center p-3">
-    <img
-      className="Logo"
-      src="https://spacetravellerhubcapstone.netlify.app/static/media/planet.3f6fb2742f28651730c0.png"
-      alt="Logo"
-      style={{ width: '50px' }}
-    />
-    <h3>Space Travelers&apos; Hub</h3>
-    <ul
-      className="list-unstyled m-2 p-2 d-flex flex-wrap"
-      style={{ justifyContent: 'flex-end', flex: '2' }}
-    >
-      {links.map((link) => (
-        <li key={link.text} className="m-2">
-          <NavLink
-            className={({ isActive }) => (isActive ? 'active-link' : 'link')}
-            to={link.path}
-          >
-            {link.text}
-          </NavLink>
-        </li>
-      ))}
-    </ul>
-  </Container>
+  <>
+    <Container className="d-flex flex-wrap align-items-center p-3">
+      <img
+        className="Logo"
+        src="https://spacetravellerhubcapstone.netlify.app/static/media/planet.3f6fb2742f28651730c0.png"
+        alt="Logo"
+        style={{ width: '50px' }}
+      />
+      <h3>Space Travelers&apos; Hub</h3>
+      <ul
+        className="list-unstyled m-2 p-2 d-flex flex-wrap"
+        style={{ justifyContent: 'flex-end', flex: '2' }}
+      >
+        {links.map((link) => (
+          <li key={link.text} className="m-2">
+            <NavLink
+              className={({ isActive }) => (isActive ? 'active-link' : 'link')}
+              to={link.path}
+            >
+              {link.text}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </Container>
+    <hr className="m-5 my-1" />
+  </>
 );
 
 export default Nav;
